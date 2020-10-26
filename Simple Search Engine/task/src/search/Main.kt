1a2e7bc215510863d7920653eeba8e99fb11c64b
alessandro0108 <alessandro0108@users.noreply.github.com>
2020-10-26 19:02:00 +0100

    Added inverted index search

--- Simple Search Engine/task/src/search/Main.kt
+++ Simple Search Engine/task/src/search/Main.kt
@@ -15,6 +15,8 @@ fun main(args: Array<String>) {
         val file = File(fileName)
         val people = file.readLines()
 
+        val index = createInvertedIndex(people)
+
         var exit = false
         while (!exit) {
             when (getMenuOptions()) {
@@ -22,7 +24,8 @@ fun main(args: Array<String>) {
                     println()
                     println("Enter a name or email to search all suitable people.")
                     val query = readLine()!!
-                    val results: List<String> = doSearch(people, query)
+                    //var results: List<String> = doLinearSearch(people, query)
+                    val results: List<String> = doInvertedIndexSearch(people, index, query)
                     if (results.isNotEmpty()) {
                         printDataSet(results)
                     } else {
@@ -49,11 +52,19 @@ fun main(args: Array<String>) {
 
 }
 
-
-private fun printDataSet(results: List<String>) {
-    for (j in results.indices) {
-        println(results[j])
+private fun createInvertedIndex(people: List<String>): MutableMap<String, MutableList<Int>> {
+    val index = mutableMapOf<String, MutableList<Int>>()
+    for (line in people.indices) {
+        val words = people[line].split(" ")
+        for (word in words) {
+            if (!index.containsKey(word)) {
+                val indexList = mutableListOf<Int>()
+                index[word] = indexList
+            }
+            index[word]?.add(line)
+        }
     }
+    return index
 }
 
 fun getMenuOptions(): String {
@@ -64,7 +75,7 @@ fun getMenuOptions(): String {
     return readLine()!!
 }
 
-fun doSearch(people: List<String>, query: String): List<String> {
+fun doLinearSearch(people: List<String>, query: String): List<String> {
     val results = mutableListOf<String>()
     for (i in people.indices) {
         if (people[i].toUpperCase().contains(query.toUpperCase())) {
@@ -74,3 +85,20 @@ fun doSearch(people: List<String>, query: String): List<String> {
     return results
 }
 
+fun doInvertedIndexSearch(people: List<String>, index: Map<String, MutableList<Int>>, query: String): List<String> {
+    val results = mutableListOf<String>()
+    for (k in index.keys) {
+        if (query == k && index[k] != null) {
+            for (line in index[k]!!) {
+                results.add(people[line])
+            }
+        }
+    }
+    return results
+}
+
+private fun printDataSet(results: List<String>) {
+    for (j in results.indices) {
+        println(results[j])
+    }
+}
